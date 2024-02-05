fcc0b68117d3cb72f8ed2dc535197eea9af6bd36
kuncarous <mscsirmaster@gmail.com>
2024-02-05 01:14:47 +0100

    moved client cmake to client folder added gameserver base

---
+++ client/game/include/ui_noesisgui_license.h
@@ -0,0 +1,10 @@
+#ifndef __UI_NOESISGUI_LICENSE_H__
+#define __UI_NOESISGUI_LICENSE_H__
+
+#pragma once
+
+/* Check https://www.noesisengine.com/docs/Gui.Core.SDKGuide.html to know how to configure this file  */
+#define NS_LICENSE_NAME "Codeline Studio"
+#define NS_LICENSE_KEY "0cPNYpKOzWLOJDlYqZ8xC8GU8x3MAQb50e19cbcrqCbpUN+a"
+
+#endif
