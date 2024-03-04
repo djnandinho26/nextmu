9b3ceed84177e1629211e23a2494079141740ea6
kuncarous <mscsirmaster@gmail.com>
2024-03-04 17:46:51 +0100

    Added CPM support Added Conan support for CMake (server only) added web system (HTTPs requests support) Added update system Added updated assets loading Added NoesisGUI intro support Added NoesisGUI localization support Added NoesisGUI richtext support Improved attachments map using CRC32 precomputed values for uint32_t key instead of string key

--- client/game/include/ui_noesisgui_license.h
+++ client/game/include/ui_noesisgui_license.h
@@ -5,6 +5,6 @@
 
 /* Check https://www.noesisengine.com/docs/Gui.Core.SDKGuide.html to know how to configure this file  */
 #define NS_LICENSE_NAME "Codeline Studio"
-#define NS_LICENSE_KEY "0cPNYpKOzWLOJDlYqZ8xC8GU8x3MAQb50e19cbcrqCbpUN+a"
+#define NS_LICENSE_KEY "/Chuu55lbrvJCJ858WKnH63nc4kkT85ns+2zP9vHtRAS1JT3"
 
 #endif
